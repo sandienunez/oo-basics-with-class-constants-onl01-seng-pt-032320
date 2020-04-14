628ef19a3c1b283fb1e6174e5be42b858d6c20b5
Sandie Nunez <snunez@vt.edu>
2020-04-14 04:19:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,24 +1,44 @@
+#class Shoe
+#  attr_accessor :color, :size, :material, :condition
+#  attr_reader :brand
+  
+#  BRANDS = ["Uggs", "Rainbow"]
+
+# def initialize(brand)
+ #   @brand = brand
+#  BRANDS << brand #unless WHAT_HERE? #unless the brand is already on BRANDS
+ # end
+  
+
+#  def cobble
+ #   self.condition = "new"
+  #  puts "Your shoe is as good as new!"
+#  end
+
+
+# end
+
+#def brands=(brands)
+   # @brands = brands
+   # BRANDS << brands 
+ # end
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  BRANDS = ["Uggs", "Rainbow"]
 
- def initialize(brand)
+  BRANDS = []
+
+  def initialize(brand)
     @brand = brand
-  BRANDS << brand #unless WHAT_HERE? #unless the brand is already on BRANDS
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
-  
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
-
 end
-
-#def brands=(brands)
-   # @brands = brands
-   # BRANDS << brands 
- # end
